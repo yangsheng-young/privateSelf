6cb15e79461429c949f5afd629ba766fd840a2af
yangsheng <yangsheng@sunwayworld.com>
2021-04-27 18:07:44 +0800

    pom、配置、主类（不正常）

---
+++ yang/sheng/young/YangInitApplication.java
@@ -0,0 +1,15 @@
+package young;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * @Author yangs
+ * @Description
+ * @Date 2021/4/27
+ */
+@YangInitApplication
+public class YangInitApplication {
+    public static void main(String[] args){
+        SpringApplication.run(YangInitApplication.class, args);
+    }
+}
