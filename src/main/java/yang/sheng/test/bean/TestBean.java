ecba7b1e8c471b1be528a0b5891ef09a2b2c8a54
swiperqi@163.com <z683030917>
2021-04-28 13:28:00 +0800

    swagger注释

--- src/main/java/yang/sheng/test/bean/TestBean.java
+++ src/main/java/yang/sheng/test/bean/TestBean.java
@@ -1,11 +1,14 @@
 package yang.sheng.test.bean;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 public class TestBean {
 
+    @ApiModelProperty("id")
     private Long id;
 
+    @ApiModelProperty("名称")
     private String testName;
 }
--- src/main/java/yang/sheng/test/controller/TestController.java
+++ src/main/java/yang/sheng/test/controller/TestController.java
@@ -1,5 +1,8 @@
 package yang.sheng.test.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,17 +13,20 @@ import yang.sheng.test.service.TestService;
 
 import java.util.List;
 
+@Api
 @RestController
 public class TestController {
 
     @Autowired
     private TestService testService;
 
+    @ApiOperation("插入数据")
     @PostMapping("/insert")
-    public void insert(@RequestBody TestBean testBean) {
+    public void insert(@RequestBody @ApiParam("数据") TestBean testBean) {
         testService.insert(testBean);
     }
 
+    @ApiOperation("查询所有数据")
     @GetMapping("/selectAll")
     public List<TestBean> selectAll() {
         return testService.selectAll();
