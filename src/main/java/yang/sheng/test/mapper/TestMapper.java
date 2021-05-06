65c5ff5e9157648451d7719fd7b946554417c5f9
yangsheng <yangsheng@sunwayworld.com>
2021-05-06 17:36:23 +0800

    pom调整

--- src/main/java/yang/sheng/test/mapper/TestMapper.java
+++ src/main/java/yang/sheng/test/mapper/TestMapper.java
@@ -2,11 +2,13 @@ package yang.sheng.test.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
 import yang.sheng.test.bean.TestBean;
 
 import java.util.List;
 
 @Mapper
+@Repository
 public interface TestMapper {
 
     void insert(TestBean testBean);
