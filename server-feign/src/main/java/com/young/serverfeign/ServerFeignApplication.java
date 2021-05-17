eb2b4512a8b3bdb5685c994999ccd0af33b47fb6
yangsheng <yangsheng@sunwayworld.com>
2021-05-17 17:46:00 +0800

    微服务分支：feign-服务消费者

---
+++ server-feign/src/main/java/com/young/serverfeign/ServerFeignApplication.java
@@ -0,0 +1,17 @@
+package com.young.serverfeign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
+public class ServerFeignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerFeignApplication.class, args);
+    }
+
+}
--- yangsheng-core/src/main/java/com/young/yangshengcore/YangshengCoreApplication.java
+++ yangsheng-core/src/main/java/com/young/yangshengcore/YangshengCoreApplication.java
@@ -2,7 +2,11 @@ package com.young.yangshengcore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RestController;
 
+@EnableEurekaClient
+@RestController
 @SpringBootApplication(scanBasePackages = "com.young")
 public class YangshengCoreApplication {
 
