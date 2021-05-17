5ebf124625726cb5a4572cd12a53ca8bbcfc5ba0
yangsheng <yangsheng@sunwayworld.com>
2021-05-17 16:07:55 +0800

    微服务：1%，提交保存下代码

---
+++ yangsheng-core/src/main/java/com/young/yangshengcore/YangshengCoreApplication.java
@@ -0,0 +1,13 @@
+package com.young.yangshengcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.young")
+public class YangshengCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YangshengCoreApplication.class, args);
+    }
+
+}
